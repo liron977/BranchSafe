8772cab4a0331d523f808581701e3f23df5868f5
Liron aviv Kotev <lironkt@mta.ac.il>
2023-11-06 00:08:38 +0200

    init

--- src/main/java/com/example/demo/Demo8Application.java
+++ src/main/java/com/example/demo/Demo8Application.java
@@ -11,13 +11,7 @@ public class Demo8Application {
 
     public static void main(String[] args) throws TelegramApiException {
         SpringApplication.run(Demo8Application.class, args);
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        Bot bot = new Bot();
-        botsApi.registerBot(bot);
-
-      /*  ChatGpt chatGpt=new ChatGpt( "https://api.openai.com/v1/chat/completions","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");
-               System.out.println(chatGpt.chatGPT("hello, how are you? Can you tell me what's a Fibonacci Number?"));*/
-       // bot.sendText(bot.userWhoSentMessage, "Hello World!");*/
+        System.out.println("Liron");
     }
 
 }
